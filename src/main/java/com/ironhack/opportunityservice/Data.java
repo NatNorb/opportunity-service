594d69a30e48f3e26eecd35e7c19c2ba0ec77ece
NatNorb <natalia.n@op.pl>
2021-11-03 22:27:48 +0100

    JDK 8

--- src/main/java/com/ironhack/opportunityservice/Data.java
+++ src/main/java/com/ironhack/opportunityservice/Data.java
@@ -17,7 +17,16 @@ public class Data {
     OpportunityRepository opportunityRepository;
 
     public void populate() throws ExceedsMaxLength {
-        List<Opportunity> opportunities = opportunityRepository.saveAll(List.of(
+        opportunityRepository.save(new Opportunity(Status.OPEN, Truck.FLATBED, 1, 1l, 2l, 1l));
+        opportunityRepository.save(new Opportunity(Status.CLOSED_LOST,Truck.BOX, 2, 2l, 2l, 2l));
+        opportunityRepository.save(new Opportunity(Status.CLOSED_LOST,Truck.HYBRID, 3, 3l, 3l, 1l));
+        opportunityRepository.save(new Opportunity(Status.OPEN,Truck.BOX, 4, 4l, 3l, 2l));
+        opportunityRepository.save(new Opportunity(Status.CLOSED_WON,Truck.HYBRID, 5, 3l, 3l, 1l));
+        opportunityRepository.save(new Opportunity(Status.CLOSED_WON,Truck.FLATBED, 10, 1l, 1l, 1l));
+        opportunityRepository.save(new Opportunity(Status.OPEN,Truck.BOX, 11, 2l, 2l, 2l));
+        opportunityRepository.save(new Opportunity(Status.OPEN,Truck.HYBRID, 15, 3l, 4l, 1l));
+
+       /* List<Opportunity> opportunities = opportunityRepository.saveAll(List.of(
                 new Opportunity(Status.OPEN, Truck.FLATBED, 1, 1l, 2l, 1l),
                 new Opportunity(Status.CLOSED_LOST,Truck.BOX, 2, 2l, 2l, 2l),
                 new Opportunity(Status.CLOSED_LOST,Truck.HYBRID, 3, 3l, 3l, 1l),
@@ -25,9 +34,8 @@ public class Data {
                 new Opportunity(Status.CLOSED_WON,Truck.HYBRID, 5, 3l, 3l, 1l),
                 new Opportunity(Status.CLOSED_WON,Truck.FLATBED, 10, 1l, 1l, 1l),
                 new Opportunity(Status.OPEN,Truck.BOX, 11, 2l, 2l, 2l),
-                new Opportunity(Status.OPEN,Truck.HYBRID, 15, 3l, 3l, 1l)
-
-        ));
+                new Opportunity(Status.OPEN,Truck.HYBRID, 15, 3l, 4l, 1l)
+        ));*/
 
     }
 }
